027cca4b00b4886a00e2df7c35ab25cfdf5274d8
rosscodingrockstars <hey@jenniferross.tech>
2020-08-05 15:58:32 -0400

    ran tests and experimented

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-alert("Okay")
+const favoriteFoods = {
+  breakfast: "french toast",
+  lunch: "cheddar bowls",
+  dinner: "turkey and mashed potatoes",
+  dessert: "brownies and icecream"
+};
+console.log(favoriteFoods.dinner);
