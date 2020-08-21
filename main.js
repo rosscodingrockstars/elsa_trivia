62316fa7e30394cb0d3aa4712f381ebed2be9699
rosscodingrockstars <hey@jenniferross.tech>
2020-08-21 15:58:15 -0400

    added gifs

--- main.js
+++ main.js
@@ -4,69 +4,80 @@ const mathProblems = [
     answerChoices: [
       "63", "72", "90", "79",
     ],
-    answer: "63"
+    answer: "63",
+    gif: "images/gif1.gif"
   },
   {
     question: "what is 12x12?",
     answerChoices: [
       "124", "100", "144", "112",
     ],
-    answer: "144"
+    answer: "144",
+    gif: "images/gif2.gif"
+
   },
   {
     question: "what is 72x4?",
     answerChoices: [
       "148", "200", "208", "288",
     ],
-    answer: "288"
+    answer: "288",
+    gif: "images/gif3.gif"
   },
   {
     question: "what is 5x11?",
     answerChoices: [
       "50", "55", "110", "45",
     ],
-    answer: "55"
+    answer: "55",
+    gif: "images/gif4.gif"
   },
   {
     question: "what is 9x17?",
     answerChoices: [
       "153", "108", "126", "97",
     ],
-    answer: "153"
+    answer: "153",
+    gif: "images/gif5.gif"
   },
   {
     question: "what is 4x6?",
     answerChoices: [
       "12", "16", "24", "36",
     ],
-    answer: "24"
+    answer: "24",
+    gif: "images/gif6.gif"
   },
   {
     question: "what is 97x4?",
     answerChoices: [
       "199", "307", "412", "388",
     ],
-    answer: "388"
+    answer: "388",
+    gif: "images/gif7.gif"
   },
   {
     question: "what is 123x5?",
     answerChoices: [
       "589", "568", "615", "613",
     ],
-    answer: "615"
+    answer: "615",
+    gif: "images/gif8.gif"
   },
   {
     question: "what is 812x2?",
     answerChoices: [
       "1579", "1509", "1628", "1624",
     ],
-    answer: "1624"
+    answer: "1624",
+    gif: "images/gif9.gif"
   },
   {
     question: "what is 672 divided by 111",
     answerChoices: [
       "6.05405405402", "6.07895405405", "8.05405405305", "6.05405405405",
     ],
-    answer: "6.05405405405"
+    answer: "6.05405405405",
+    gif: "images/gif10.gif"
   },
 ]
