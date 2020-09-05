96cbe50a36f27d108851ac4eaf3352bcd0449cd9
rosscodingrockstars <hey@jenniferross.tech>
2020-09-05 12:18:26 -0400

    Adding form

--- main.js
+++ main.js
@@ -1,88 +1,71 @@
 const mathProblems = [
   {
     question: "what is 7x9?",
-    answerChoices: [
-      "63", "72", "90", "79",
-    ],
+    answerChoices: ["63", "72", "90", "79"],
     answer: "63",
     gif: "images/gif1.gif"
   },
   {
     question: "what is 12x12?",
-    answerChoices: [
-      "124", "100", "144", "112",
-    ],
+    answerChoices: ["124", "100", "144", "112"],
     answer: "144",
     gif: "images/gif2.gif"
-
   },
   {
     question: "what is 72x4?",
-    answerChoices: [
-      "148", "200", "208", "288",
-    ],
+    answerChoices: ["148", "200", "208", "288"],
     answer: "288",
     gif: "images/gif3.gif"
   },
   {
     question: "what is 5x11?",
-    answerChoices: [
-      "50", "55", "110", "45",
-    ],
+    answerChoices: ["50", "55", "110", "45"],
     answer: "55",
     gif: "images/gif4.gif"
   },
   {
     question: "what is 9x17?",
-    answerChoices: [
-      "153", "108", "126", "97",
-    ],
+    answerChoices: ["153", "108", "126", "97"],
     answer: "153",
     gif: "images/gif5.gif"
   },
   {
     question: "what is 4x6?",
-    answerChoices: [
-      "12", "16", "24", "36",
-    ],
+    answerChoices: ["12", "16", "24", "36"],
     answer: "24",
     gif: "images/gif6.gif"
   },
   {
     question: "what is 97x4?",
-    answerChoices: [
-      "199", "307", "412", "388",
-    ],
+    answerChoices: ["199", "307", "412", "388"],
     answer: "388",
     gif: "images/gif7.gif"
   },
   {
     question: "what is 123x5?",
-    answerChoices: [
-      "589", "568", "615", "613",
-    ],
+    answerChoices: ["589", "568", "615", "613"],
     answer: "615",
     gif: "images/gif8.gif"
   },
   {
     question: "what is 812x2?",
-    answerChoices: [
-      "1579", "1509", "1628", "1624",
-    ],
+    answerChoices: ["1579", "1509", "1628", "1624"],
     answer: "1624",
     gif: "images/gif9.gif"
   },
   {
     question: "what is 672 divided by 111",
     answerChoices: [
-      "6.05405405402", "6.07895405405", "8.05405405305", "6.05405405405",
+      "6.05405405402",
+      "6.07895405405",
+      "8.05405405305",
+      "6.05405405405"
     ],
     answer: "6.05405405405",
     gif: "images/gif10.gif"
-  },
-]
+  }
+];
 
-console.log(mathProblems[0].answerc)
+console.log(mathProblems[0].answerc);
 
-
-const Question
+const Question = `<form><div class="form-group"><label for="exampleInputEmail1">Email address</label><input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"><small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>  </div><div class="form-group"><label for="exampleInputPassword1">Password</label><input type="password" class="form-control" id="exampleInputPassword1"></div><div class="form-group form-check"><input type="checkbox" class="form-check-input" id="exampleCheck1"><label class="form-check-label" for="exampleCheck1">Check me out</label>  </div><button type="submit" class="btn btn-primary">Submit</button></form>`;
