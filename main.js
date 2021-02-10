cd7a28f9ed5e01ff5614e62436e90aa5e86ac146
rosscodingrockstars <hey@jenniferross.tech>
2021-02-10 16:08:06 -0500

    changed setup

--- main.js
+++ main.js
@@ -1,67 +1,67 @@
 const mathProblems = [
   {
     question: "what is 7x9?",
-    answerChoices: ["63", "72", "90", "79"],
-    answer: "63",
+    answerChoices: [63, 72, 90, 79],
+    answer: 63,
     gif: "images/gif1.gif",
   },
   {
     question: "what is 12x12?",
-    answerChoices: ["124", "100", "144", "112"],
-    answer: "144",
+    answerChoices: [124, 100, 144, 112],
+    answer: 124,
     gif: "images/gif2.gif",
   },
   {
     question: "what is 72x4?",
-    answerChoices: ["148", "200", "208", "288"],
-    answer: "288",
+    answerChoices: [148, 200, 208, 288],
+    answer: 288,
     gif: "images/gif3.gif",
   },
   {
     question: "what is 5x11?",
-    answerChoices: ["50", "55", "110", "45"],
-    answer: "55",
+    answerChoices: [50, 55, 110, 45],
+    answer: 55,
     gif: "images/gif4.gif",
   },
   {
     question: "what is 9x17?",
-    answerChoices: ["153", "108", "126", "97"],
-    answer: "153",
+    answerChoices: [153, 108, 26, 97],
+    answer: 153,
     gif: "images/gif5.gif",
   },
   {
     question: "what is 4x6?",
-    answerChoices: ["12", "16", "24", "36"],
-    answer: "24",
+    answerChoices: [12, 16, 24, 36],
+    answer: 24,
     gif: "images/gif6.gif",
   },
   {
     question: "what is 97x4?",
-    answerChoices: ["199", "307", "412", "388"],
-    answer: "388",
+    answerChoices: [199, 307, 412, 388],
+    answer: 388,
     gif: "images/gif7.gif",
   },
   {
     question: "what is 123x5?",
-    answerChoices: ["589", "568", "615", "613"],
-    answer: "615",
+    answerChoices: [589, 568, 615, 613],
+    answer: 615,
     gif: "images/gif8.gif",
   },
   {
     question: "what is 812x2?",
-    answerChoices: ["1579", "1509", "1628", "1624"],
-    answer: "1624",
+    answerChoices: [1579, 1509, 1628, 1624],
+    answer: 1624,
     gif: "images/gif9.gif",
   },
   {
     question: "what is 672 divided by 111",
     answerChoices: [
-      "6.05405405402",
-      "6.07895405405",
-      "8.05405405305",
-      "6.05405405405",
+      6.05405405402,
+      6.07895405405,
+      8.05405405305,
+      6.05405405405,
     ],
-    answer: "6.05405405405",
+    answer: 6.05405405405,
     gif: "images/gif10.webp",
   },
 ];
@@ -111,7 +111,7 @@ function handleQandA() {
 function handleSubmit() {
   $(".submit").click(function (e) {
     e.preventDefault();
-    let answer = `${mathProblems[0].answer}`;
+    let answer = `${mathProblems[questionCount].answer}`;
     if (answer === $("input:checked").val()) {
       correct();
     } else {
@@ -165,13 +165,14 @@ $(".gif").show();
   $(".gif-container").append(
     $(".gif").attr("src", "images/giphy.gif")
   );
-
+  $(".play-again").show();
 
 
 }
 function startQuiz() {
   $(".gif").hide();
   $(".next").hide();
+  $(".play-again").hide();
   questionCount = 0;
   score = 0;
   handleQandA();
